014f56034e54f18098a9cfc3b7223298809119a4
Emmet Moore <x21192090@student.ncirl.ie>
2023-03-14 16:21:06 +0100

    disabled devtools for production

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  define: { __VUE_PROD_DEVTOOLS__: true }
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   },
+//   define: { __VUE_PROD_DEVTOOLS__: true }
+// })
+
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    define: { __VUE_PROD_DEVTOOLS__: command === 'serve' }
+  }
 })
